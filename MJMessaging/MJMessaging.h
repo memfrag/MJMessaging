47e203086a6c07e03e836acc413926c38aa9dce7
Martin Johannesson <martin.johannesson@bontouch.com>
2012-06-22 14:18:53 +0200

    Added GCDAsyncSocket.

--- MJMessaging/MJMessaging.h
+++ MJMessaging/MJMessaging.h
@@ -3,11 +3,8 @@
 //  MJMessaging
 //
 //  Created by Martin Johannesson on 2012-06-22.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012 Martin Johannesson. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
-@interface MJMessaging : NSObject
-
-@end
