40a139f2ecc090d52af1aab2a4dfbfffc4f7959d
Martin Johannesson <martin.johannesson@bontouch.com>
2012-06-22 14:07:40 +0200

    Initial Commit

---
+++ MJMessaging/MJMessaging.h
@@ -0,0 +1,13 @@
+//
+//  MJMessaging.h
+//  MJMessaging
+//
+//  Created by Martin Johannesson on 2012-06-22.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MJMessaging : NSObject
+
+@end
